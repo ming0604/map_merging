4bf20c83b456626bd24b01584db6f2acc5a9ad9c
ming0604 <b0981340647@gmail.com>
2025-03-19 19:52:34 +0800

    can output feature image and add feature type & output directory parameters

--- src/feature_matching_test.cpp
+++ src/feature_matching_test.cpp
@@ -2,8 +2,17 @@
 #include "map_merging/map_merging_tool.h"  // Adjust path according to your package structure
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+// Funtion to find the file name without extension
+string getFileName(const string& path)
+{
+    size_t start = path.find_last_of("/\\");
+    size_t end = path.find_last_of(".");
+    return path.substr(start + 1, end - start - 1);
+}
 
 int main(int argc, char** argv)
 {
@@ -12,14 +21,18 @@ int main(int argc, char** argv)
     ros::NodeHandle nh("~");
 
     // Parameter check: two image file paths are required as input
-    if (argc < 3)
+    if (argc < 5)
     {
-        cerr << "Usage: " << argv[0] << " <map1_image> <map2_image>" << endl;
+        cerr << "Usage: " << argv[0] << " <map1_image_path> <map2_image_path> <output_folder_path> <feature_type> " << endl;
         return -1;
     }
 
     string map1Path = argv[1];
     string map2Path = argv[2];
+    // Set the output directory
+    string save_dir = argv[3];
+    string feature_type = argv[4];
+    string map1_file_name, map2_file_name;
 
     // Read the two map images
     cv::Mat map1 = cv::imread(map1Path, cv::IMREAD_GRAYSCALE);
@@ -29,21 +42,49 @@ int main(int argc, char** argv)
         cerr << "Error: Cannot read image " << map1Path << " or " << map2Path << endl;
         return -1;
     }
+    // Extract map filenames (without extension) for output naming
+    map1_file_name = getFileName(map1Path);
+    map2_file_name = getFileName(map2Path);
+    // Print the names of the input maps
+    cout << "Map1: " << map1_file_name << endl;
+    cout << "Map2: " << map2_file_name << endl;
+ 
 
-    // Create a feature detector and BFMatcher
-    // For SIFT use NORM_L2 with crossCheck enabled
-    cv::Ptr<cv::Feature2D> detector = cv::SIFT::create();
-    // Alternatively, to use AKAZE or ORB, uncomment the following line:
-    // cv::Ptr<cv::Feature2D> detector = cv::AKAZE::create();
-    // cv::Ptr<cv::Feature2D> detector = cv::ORB::create();
-    ROS_INFO("Feature detector type: %s", detector->getDefaultName().c_str());
+    // Create the feature detector and BFMatcher
+    cv::Ptr<cv::Feature2D> detector;
+    cv::Ptr<cv::DescriptorMatcher> matcher;
+    if(feature_type == "SIFT")
+    {   
+        // For SIFT use NORM_L2 
+        detector = cv::SIFT::create();
+        //matcher = cv::BFMatcher::create(cv::NORM_L2, true); // crossCheck enabled
+        matcher = cv::BFMatcher::create(cv::NORM_L2, false); // crossCheck disabled
+    }
+    else if(feature_type == "AKAZE")
+    {   
+        // For AKAZE or ORB,use NORM_HAMMING:
+        detector = cv::AKAZE::create();
+        //matcher = cv::BFMatcher::create(cv::NORM_HAMMING, true); // crossCheck enabled
+        matcher = cv::BFMatcher::create(cv::NORM_HAMMING, false); // crossCheck disabled
+    }
+    else if(feature_type == "ORB")
+    {   
+        // For AKAZE or ORB,use NORM_HAMMING:
+        detector = cv::ORB::create(3000);
+        //matcher = cv::BFMatcher::create(cv::NORM_HAMMING, true); // crossCheck enabled
+        matcher = cv::BFMatcher::create(cv::NORM_HAMMING, false); // crossCheck disabled
+    }
+    else
+    {
+        cerr << "Error: Invalid feature type. Please choose from SIFT, AKAZE, or ORB." << endl;
+        return -1;
+    }
+    // Print the feature type
+    ROS_INFO("Feature type: %s", detector->getDefaultName().c_str());
 
-    cv::Ptr<cv::DescriptorMatcher> matcher = cv::BFMatcher::create(cv::NORM_L2, true);
-    // For AKAZE or ORB,use NORM_HAMMING:
-    //cv::Ptr<cv::DescriptorMatcher> matcher = cv::BFMatcher::create(cv::NORM_HAMMING, true);
-    
     // Create a MapMergingTool object, passing in the detector and matcher
-    MapMergingTool tool(detector, matcher);
+    float ratio_threshold = 0.7;    
+    MapMergingTool tool(detector, matcher, ratio_threshold);
 
     // Perform feature detection
     vector<cv::KeyPoint> kp1, kp2;
@@ -52,21 +93,40 @@ int main(int argc, char** argv)
     tool.detect_features(map2, kp2, desc2);
 
     // Draw the detected features on each image
-    cv::Mat map1Features = tool.draw_features(map1, kp1);
-    cv::Mat map2Features = tool.draw_features(map2, kp2);
+    cv::Mat map1_with_features = tool.draw_features(map1, kp1);
+    cv::Mat map2_with_features = tool.draw_features(map2, kp2);
+    // Print the number of detected features
+    cout << "Number of features in map1: " << kp1.size() << endl;
+    cout << "Number of features in map2: " << kp2.size() << endl;
+    // Display the images with features
+    cv::imshow("Map1 Features", map1_with_features);
+    cv::imshow("Map2 Features", map2_with_features);
 
-    cv::imshow("Map1 Features", map1Features);
-    cv::imshow("Map2 Features", map2Features);
-
+    /*
+    // Save the images with features
+    string map1_with_features_path = save_dir + map1_file_name + "_" + feature_type + "_features.png";
+    string map2_with_features_path = save_dir + map2_file_name + "_" + feature_type + "_features.png";
+    cv::imwrite(map1_with_features_path, map1_with_features);
+    cv::imwrite(map2_with_features_path, map2_with_features);
+    cout << "Saved images with features to " << save_dir << endl;
+    */
     // Perform feature matching
     vector<cv::DMatch> matches;
     tool.match_features(desc1, desc2, matches);
-    cout << "[INFO] Number of matches: " << matches.size() << endl;
+    cout << "Number of matches: " << matches.size() << endl;
 
     // Draw the matching results (here all matches are drawn; you can modify to show only the top N matches if needed)
-    cv::Mat matchImg = tool.draw_matches(map1, kp1, map2, kp2, matches);
-    cv::imshow("Match Result", matchImg);
-
+    cv::Mat match_img = tool.draw_matches(map1, kp1, map2, kp2, matches);
+    cv::imshow("Match Result", match_img);
+    /*
+    
+    // Save the matching result image
+    //string match_img_path = save_dir + map1_file_name + "_" + map2_file_name + "_" + feature_type + "_matches_crosscheck.png";
+    //string match_img_path = save_dir + map1_file_name + "_" + map2_file_name + "_" + feature_type + "_matches_no_crosscheck.png";
+    string match_img_path = save_dir + map1_file_name + "_" + map2_file_name + "_" + feature_type + "_matches_ratio_test.png";
+    cv::imwrite(match_img_path, match_img);
+    cout << "Saved matching result image to " << save_dir << endl;
+    */
     cv::waitKey(0);
     cv::destroyAllWindows();
 

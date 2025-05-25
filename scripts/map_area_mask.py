28c9f2b4055804e8a3fb2378bcf342d13e35d1a5
ming0604 <b0981340647@gmail.com>
2025-05-26 04:43:08 +0800

    map area masking to unknown by manually selection

---
+++ scripts/map_area_mask.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+import cv2
+import numpy as np
+import os
+import sys
+
+# global variables
+rectangles = []
+map_rect_area_img = None
+rect_start = None
+drawing = False
+
+def generate_masked_map(map_raw_img, map_yaml_path, output_map_img_path, output_map_yaml_path):
+    global rectangles
+    # Mask the selected rectangles areas to 205 (means unknown area in the ROS map)
+    masked_map = map_raw_img.copy()
+    for rect in rectangles:
+        (x_min, y_min), (x_max, y_max) = rect
+        # row is y, column is x in the image, tail +1 to include the last pixel
+        masked_map[y_min:y_max+1, x_min:x_max+1] = 205
+    cv2.imwrite(output_map_img_path, masked_map)
+    print(f"Masked map image is saved to {output_map_img_path}")
+    print(f"Size of the masked map: h:{masked_map.shape[0]}, w:{masked_map.shape[1]}")    
+
+    # Save the masked map yaml file
+    # Check if the yaml file exists
+    if not os.path.exists(map_yaml_path):
+        print(f"Error: The map's yaml file does not exist, please ensure the name of the yaml file is same as the map image file.")
+        sys.exit(1)
+    # Read the yaml file of the original map
+    with open(map_yaml_path, 'r') as f:
+        original_yaml_lines = f.readlines()
+    #　Create a new yaml file for the masked map
+    with open(output_map_yaml_path, 'w') as f:
+        for line in original_yaml_lines:
+            # Replace the image file path with the new masked map image file path 
+            # Check if the line starts with "image:"
+            if line.strip().startswith("image:"):      
+                f.write(f"image: {output_map_img_path}\n")
+            # Copy other lines as they are
+            else:
+                f.write(line)
+    print(f"Masked map yaml file is saved to {output_map_yaml_path}")
+
+def draw_rectangles(event, x, y, flags, param):
+    global rectangles, map_rect_area_img, rect_start, drawing
+    # Left mouse button down to start drawing a rectangle area
+    if event == cv2.EVENT_LBUTTONDOWN:
+        # Store the starting point of the rectangle
+        rect_start = (x, y)
+        # Set the drawing flag to True
+        drawing = True
+    # Mouse move to choose the rectangle area for masking
+    elif event == cv2.EVENT_MOUSEMOVE and drawing:
+        temp_img = map_rect_area_img.copy()
+        # Draw the rectangle you are currently selecting
+        cv2.rectangle(temp_img, rect_start, (x, y), (0, 0, 255), 2)
+        cv2.imshow("Map", temp_img)
+    # Left mouse button up to finish drawing this rectangle area
+    elif event == cv2.EVENT_LBUTTONUP:
+        # Set the drawing flag to False
+        drawing = False
+        # Store the rectangle into the list of rectangles
+        x_start, y_start = rect_start
+        # Ensure the rectangle coordinates are in the correct order
+        x_min = min(x_start, x)
+        x_max = max(x_start, x)
+        y_min = min(y_start, y) 
+        y_max = max(y_start, y)
+        rectangles.append(((x_min, y_min), (x_max, y_max)))
+        # Print the added rectangle coordinates
+        print(f"Added selected area: x_min={x_min}, y_min={y_min}, x_max={x_max}, y_max={y_max}")
+        # Draw the rectangle on the map image
+        # Update the  map rect area image with the new rectangle
+        cv2.rectangle(map_rect_area_img, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
+        cv2.imshow("Map", map_rect_area_img)
+def main():
+    global rectangles, map_rect_area_img
+  
+    map_folder_path = "/home/mingzhun/lab_localization/src/map_merging/maps/area_mask_maps/"
+    map_name = "20241218_vive_mapping_test3"
+    map_img_path = map_folder_path + map_name + ".pgm"
+    map_yaml_path = map_folder_path + map_name + ".yaml"
+    output_map_img_name = map_name + "_masked.pgm"
+    output_map_img_path = map_folder_path + output_map_img_name
+    output_map_yaml_name = map_name + "_masked.yaml"
+    output_map_yaml_path = map_folder_path + output_map_yaml_name
+
+    # Load the map image
+    map_raw_img = cv2.imread(map_img_path, cv2.IMREAD_GRAYSCALE)
+    print(f"Loaded map image from {map_img_path}")
+    print(f"Size of the raw map: h:{map_raw_img.shape[0]}, w:{map_raw_img.shape[1]}")
+    map_rect_area_img = map_raw_img.copy()
+    
+    # Create a window to display the map
+    cv2.namedWindow("Map", cv2.WINDOW_NORMAL)
+    # Set mouse callback function to handle rectangle area marking
+    cv2.setMouseCallback("Map", draw_rectangles,)
+    # Display the map image
+    cv2.imshow("Map", map_rect_area_img)
+    print("Click and drag to select rectangle areas you want to mask.")
+    print("Press 'q' to quit and save the masked map.")
+
+    while True:
+        # Wait for a key press
+        key = cv2.waitKey(1) & 0xFF
+        # If 'q' is pressed, exit the loop
+        if key == ord('q'):
+            generate_masked_map(map_raw_img, map_yaml_path, output_map_img_path, output_map_yaml_path)
+            break
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
